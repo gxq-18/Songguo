d7f98aa6986fa79941f973c9e4e4a370b75ce023
gxq <2256730087@qq.com>
2019-11-19 16:50:59 +0800

    123
    123

--- pages/teacher/xiangqing/xiangqing.js
+++ pages/teacher/xiangqing/xiangqing.js
@@ -427,13 +427,34 @@ Page({
       })
     }.bind(this), 200)
   },
-  onShareAppMessage: function (res) {
-    return {
-      title: '十艺教育小程序测试',
-      path: '/pages/teacher/xiangqing/xiangqing?id=' + this.data.prentid,
-      imageUrl: circleList[0]//自定义图片路径，可以是本地文件路径、代码包文件路径或者网络图片路径。支持PNG及JPG。显       示图片长宽比是 5:4。
-    }
-  },
+  onShareAppMessage: function (options) {
+    var that = this;
+    // 设置菜单中的转发按钮触发转发事件时的转发内容
+    var shareObj = {
+      title: "转发的标题",    // 默认是小程序的名称(可以写slogan等)
+      path: '/pages/teacher/parentCircleListBypublic/parentCircleListBypublic?id=' + that.data.prentid+'&fx=h',    // 默认是当前页面，必须是以‘/'开头的完整路径
+      imageUrl: '',   //自定义图片路径，可以是本地文件路径、代码包文件路径或者网络图片路径，支持PNG及JPG，不传入 imageUrl 则使用默认截图。显示图片长宽比是 5:4
+      success: function (res) {
+        // 转发成功之后的回调
+        if (res.errMsg == 'shareAppMessage:ok') {
+        }
+      },
+      fail: function () {
+        // 转发失败之后的回调
+        if (res.errMsg == 'shareAppMessage:fail cancel') {
+          // 用户取消转发
+        } else if (res.errMsg == 'shareAppMessage:fail') {
+          // 转发失败，其中 detail message 为详细失败信息
+        }
+      },
+      complete: function(){
+        // 转发结束之后的回调（转发成不成功都会执行）
+      }
+  };
+  
+// 返回shareObj
+return shareObj;
+},
   //查询位置
   openMap: function (e) {
     var latitude = parseFloat(e.currentTarget.dataset.latitude);
